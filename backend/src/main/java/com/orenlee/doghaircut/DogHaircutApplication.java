d7965420f53771923d76486b7a5f1f4bee1b1907
Oren <orenlee8@gmail.com>
2022-04-27 17:54:28 +0300

    init

---
+++ backend/src/main/java/com/orenlee/doghaircut/DogHaircutApplication.java
@@ -0,0 +1,13 @@
+package com.orenlee.doghaircut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogHaircutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogHaircutApplication.class, args);
+	}
+
+}
